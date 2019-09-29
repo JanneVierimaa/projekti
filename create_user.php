0da6ff3496efa78bc6f9f9aa368692a00985bfe7
Janne Vierimaa <t8vija01@students.oamk.fi>
2019-09-29 23:06:36 +0300

    Käyttäjän rekisteröinti MariaDB-tietokantaan tehty. User taulu MariaDB-kannassa.

---
+++ create_user.php
@@ -0,0 +1,11 @@
+<?php  include "menu.php";?>
+<div class="createuser">
+
+
+<h3>Rekisteröidy</h3><br>
+<form class="" action="add_user.php" method="post">
+  <input type="text" name="username" value="" placeholder="käyttäjänimi"> <br> <br>
+  <input type="text" name="password" value="" placeholder="salasana"> <br> <br>
+  <input type="submit" name="" value="Rekisteröidy">
+</form>
+</div>
