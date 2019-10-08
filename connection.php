7bc274a4e413060c720713486cfb18b4afcac3b0
Janne Vierimaa <t8vija01@students.oamk.fi>
2019-10-08 23:10:27 +0300

    Tuote sivua parannettu: hakee tietokannasta kategorioittain

--- connection.php
+++ connection.php
@@ -1,6 +1,8 @@
 <?php
 	try
 	{
+	 //$dsn = "mysql:host=localhost;dbname=opisk_t8vija01";
+ 	 //$db = new PDO ($dsn, "phpUser", "phpPass");
 	 $dsn = "mysql:host=mysli.oamk.fi;dbname=opisk_t8vija01";
 	 $db = new PDO ($dsn, "t8vija01", "amorphis");
 	 print ("Connected\n");
